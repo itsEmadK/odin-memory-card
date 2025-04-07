65966284dd1dc0afa2a721594e4a5a96b5ed527c
Emad <67466634+itsEmadK@users.noreply.github.com>
2025-04-07 19:54:18 +0330

    Add authors array

---
+++ src/data/authors.js
@@ -0,0 +1,43 @@
+import dostoevsky from '../assets/images/dostoevsky.webp';
+import proust from '../assets/images/proust.webp';
+import twain from '../assets/images/twain.webp';
+import tolstoy from '../assets/images/tolstoy.webp';
+import sartre from '../assets/images/sartre.webp';
+import camus from '../assets/images/camus.webp';
+import hedayat from '../assets/images/hedayat.webp';
+import nietzsche from '../assets/images/nietzsche.webp';
+import melville from '../assets/images/melville.webp';
+import turgenev from '../assets/images/turgenev.webp';
+import france from '../assets/images/france.webp';
+import freud from '../assets/images/freud.webp';
+import heidger from '../assets/images/heidger.webp';
+import hugo from '../assets/images/hugo.webp';
+import marx from '../assets/images/marx.webp';
+import engels from '../assets/images/engels.webp';
+import gorki from '../assets/images/gorki.webp';
+import dumas from '../assets/images/dumas.webp';
+import chekhov from '../assets/images/chekhov.webp';
+import hemingway from '../assets/images/hemingway.webp';
+
+export const authors = [
+  { id: 0, name: 'Dostoevsky', image: dostoevsky },
+  { id: 1, name: 'Proust', image: proust },
+  { id: 2, name: 'twain', image: twain },
+  { id: 3, name: 'Proust', image: tolstoy },
+  { id: 4, name: 'Proust', image: sartre },
+  { id: 5, name: 'Proust', image: camus },
+  { id: 6, name: 'Proust', image: hedayat },
+  { id: 7, name: 'Proust', image: heidger },
+  { id: 8, name: 'Proust', image: nietzsche },
+  { id: 9, name: 'Proust', image: hemingway },
+  { id: 10, name: 'Proust', image: chekhov },
+  { id: 11, name: 'Proust', image: gorki },
+  { id: 12, name: 'Proust', image: dumas },
+  { id: 13, name: 'Proust', image: france },
+  { id: 14, name: 'Proust', image: melville },
+  { id: 15, name: 'Proust', image: turgenev },
+  { id: 16, name: 'Proust', image: engels },
+  { id: 17, name: 'Proust', image: marx },
+  { id: 18, name: 'Proust', image: freud },
+  { id: 19, name: 'Proust', image: hugo },
+];
